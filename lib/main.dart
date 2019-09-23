74a63ccce34eecc5514547f773b36a09c47b9cfd
eungikimme <eungikim.me@gmail.com>
2019-09-24 00:16:40 +0900

    feat: change Stateless to Stateful at Chatscreen
    Add TextFild and IconButton

--- lib/main.dart
+++ lib/main.dart
@@ -20,10 +20,46 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(title: Text("Friendlychat")),
+      body: _buildTextComposer(),
     );
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController ,
+                onSubmitted: _handleSubmitted,
+                decoration: InputDecoration.collapsed(
+                  hintText: "Send a message",
+                ),
+              )
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            )
+          ],
+        )
+      )
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
   }
 }
